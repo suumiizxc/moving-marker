d9d7d7e9fc86768e0e802fd0d7ac1c87de069337
suumiizxc <ch.sumyadorj@gmail.com>
2023-03-15 00:30:36 +0800

    update

--- examples/script.js
+++ examples/script.js
@@ -1,4 +1,32 @@
 // initialize the map on the "map" div with a given center and zoom
+var targetArray = [];
+
+$.ajax({
+    url: "http://localhost:8080/data",
+    type: 'GET',
+    datatype:'jsonp', 
+    success: function(res) {
+        var mydata = JSON.parse(res)['data']
+        console.log("data : ", mydata.length);
+        var loopVal = 0;
+        timeout();
+            function timeout() {
+                setTimeout(function () {
+                    // console.log("testing : ", loopVal, " array : ", mydata[loopVal]);
+                    targetArray.push([48.8567 + 0.2 * mydata[loopVal].x, 2.35 + 2 * mydata[loopVal].y]);
+                    console.log('target Array : ', targetArray);
+                    loopVal++;
+                    timeout();
+                }, 1000);
+            }
+        
+    },
+    error : function(err) {
+        console.log(err);
+    }
+});
+
+
 var map = new L.Map('map', {
   zoom: 6,
   minZoom: 3,
@@ -18,23 +46,23 @@ map.addLayer(layer);
 var parisKievLL = [[48.8567, 2.3508], [50.45, 30.523333]];
 var londonParisRomeBerlinBucarest = [[51.507222, -0.1275], [48.8567, 2.3508],
 [41.9, 12.5], [52.516667, 13.383333], [44.4166,26.1]];
-var londonBrusselFrankfurtAmsterdamLondon = [[51.507222, -0.1275], [50.85, 4.35],
-[50.116667, 8.683333], [52.366667, 4.9], [51.507222, -0.1275]];
-var barcelonePerpignanPauBordeauxMarseilleMonaco = [
-    [41.385064, 2.173403],
-    [42.698611, 2.895556],
-    [43.3017, -0.3686],
-    [44.837912, -0.579541],
-    [43.296346, 5.369889],
-    [43.738418, 7.424616]
-];
+// var londonBrusselFrankfurtAmsterdamLondon = [[51.507222, -0.1275], [50.85, 4.35],
+// [50.116667, 8.683333], [52.366667, 4.9], [51.507222, -0.1275]];
+// var barcelonePerpignanPauBordeauxMarseilleMonaco = [
+//     [41.385064, 2.173403],
+//     [42.698611, 2.895556],
+//     [43.3017, -0.3686],
+//     [44.837912, -0.579541],
+//     [43.296346, 5.369889],
+//     [43.738418, 7.424616]
+// ];
 
 
 map.fitBounds(londonParisRomeBerlinBucarest);
 
 //========================================================================
-var marker1 = L.Marker.movingMarker(parisKievLL, [10000]).addTo(map);
-L.polyline(parisKievLL).addTo(map);
+var marker1 = L.Marker.movingMarker(targetArray, [10000]).addTo(map);
+L.polyline(targetArray).addTo(map);
 marker1.once('click', function () {
     marker1.start();
     marker1.closePopup();
@@ -56,64 +84,64 @@ marker1.openPopup();
 
 //========================================================================
 
-var marker2 = L.Marker.movingMarker(londonParisRomeBerlinBucarest,
-    [3000, 9000, 9000, 4000], {autostart: true}).addTo(map);
-L.polyline(londonParisRomeBerlinBucarest, {color: 'red'}).addTo(map);
+// var marker2 = L.Marker.movingMarker(londonParisRomeBerlinBucarest,
+//     [3000, 9000, 9000, 4000], {autostart: true}).addTo(map);
+// L.polyline(londonParisRomeBerlinBucarest, {color: 'red'}).addTo(map);
 
 
-marker2.on('end', function() {
-    marker2.bindPopup('<b>Welcome to Bucarest !</b>', {closeOnClick: false})
-    .openPopup();
-});
+// marker2.on('end', function() {
+//     marker2.bindPopup('<b>Welcome to Bucarest !</b>', {closeOnClick: false})
+//     .openPopup();
+// });
+
+// //=========================================================================
+
+// var marker3 = L.Marker.movingMarker(londonBrusselFrankfurtAmsterdamLondon,
+//     [2000, 2000, 2000, 2000], {autostart: true, loop: true}).addTo(map);
+
+// marker3.loops = 0;
+// marker3.bindPopup('', {closeOnClick: false});
+
+// //=========================================================================
+
+// var marker4 = L.Marker.movingMarker([[45.816667, 15.983333]], []).addTo(map);
+
+// marker3.on('loop', function(e) {
+//     marker3.loops++;
+//     if (e.elapsedTime < 50) {
+//         marker3.getPopup().setContent("<b>Loop: " + marker3.loops + "</b>")
+//         marker3.openPopup();
+//         setTimeout(function() {
+//             marker3.closePopup();
+
+//             if (! marker1.isEnded()) {
+//                 marker1.openPopup();
+//             } else {
+//                 if (marker4.getLatLng().equals([45.816667, 15.983333])) {
+//                     marker4.bindPopup('Click on the map to move me !');
+//                     marker4.openPopup();
+//                 }
+
+//             }
+
+//         }, 2000);
+//     }
+// });
+
+// map.on("click", function(e) {
+//     marker4.moveTo(e.latlng, 2000);
+// });
 
 //=========================================================================
 
-var marker3 = L.Marker.movingMarker(londonBrusselFrankfurtAmsterdamLondon,
-    [2000, 2000, 2000, 2000], {autostart: true, loop: true}).addTo(map);
+// var marker5 = L.Marker.movingMarker(
+//     barcelonePerpignanPauBordeauxMarseilleMonaco,
+//     10000, {autostart: true}).addTo(map);
 
-marker3.loops = 0;
-marker3.bindPopup('', {closeOnClick: false});
+// marker5.addStation(1, 2000);
+// marker5.addStation(2, 2000);
+// marker5.addStation(3, 2000);
+// marker5.addStation(4, 2000);
 
-//=========================================================================
-
-var marker4 = L.Marker.movingMarker([[45.816667, 15.983333]], []).addTo(map);
-
-marker3.on('loop', function(e) {
-    marker3.loops++;
-    if (e.elapsedTime < 50) {
-        marker3.getPopup().setContent("<b>Loop: " + marker3.loops + "</b>")
-        marker3.openPopup();
-        setTimeout(function() {
-            marker3.closePopup();
-
-            if (! marker1.isEnded()) {
-                marker1.openPopup();
-            } else {
-                if (marker4.getLatLng().equals([45.816667, 15.983333])) {
-                    marker4.bindPopup('Click on the map to move me !');
-                    marker4.openPopup();
-                }
-
-            }
-
-        }, 2000);
-    }
-});
-
-map.on("click", function(e) {
-    marker4.moveTo(e.latlng, 2000);
-});
-
-//=========================================================================
-
-var marker5 = L.Marker.movingMarker(
-    barcelonePerpignanPauBordeauxMarseilleMonaco,
-    10000, {autostart: true}).addTo(map);
-
-marker5.addStation(1, 2000);
-marker5.addStation(2, 2000);
-marker5.addStation(3, 2000);
-marker5.addStation(4, 2000);
-
-L.polyline(barcelonePerpignanPauBordeauxMarseilleMonaco,
-    {color: 'green'}).addTo(map);
+// L.polyline(barcelonePerpignanPauBordeauxMarseilleMonaco,
+//     {color: 'green'}).addTo(map);
